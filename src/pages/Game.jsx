f2b9bf196097b491348617e467c0257b54afcc2b
Rocket <robertmsbutler@googlemail.com>
2026-04-18 18:58:05 +0000

    File changes

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -125,6 +125,7 @@ export default function Game() {
                 const walletAddress = user.walletAddress;
                 
                 const scoreData = {
+                    user_id: user.id,
                     player_name: displayName,
                     player_title: user.data?.player_title || '',
                     pilot_icon: pilotIcon,
