fec79d0ab39165e3598dbd9b01eacfede9648acc
Rocket <robertmsbutler@googlemail.com>
2026-04-19 16:51:29 +0000

    File changes

--- base44/functions/fixSquads/entry.ts
+++ base44/functions/fixSquads/entry.ts
@@ -3,6 +3,11 @@ import { createClientFromRequest } from 'npm:@base44/sdk@0.8.24';
 Deno.serve(async (req) => {
     try {
         const base44 = createClientFromRequest(req);
+        const user = await base44.auth.me();
+
+        if (user?.role !== 'admin') {
+            return Response.json({ error: 'Forbidden: Admin access required' }, { status: 403 });
+        }
         
         const allSquads = await base44.asServiceRole.entities.Squad.list('-created_date', 1000);
         const allMembers = await base44.asServiceRole.entities.SquadMember.list('-created_date', 5000);
