ccfaabcf5002a91f4a0b3ea2a8432589a08c8113
Rocket <robertmsbutler@googlemail.com>
2026-04-26 16:14:27 +0000

    File changes

--- base44/functions/getOrSpawnWeeklyBoss/entry.ts
+++ base44/functions/getOrSpawnWeeklyBoss/entry.ts
@@ -1,6 +1,4 @@
-import { createClient } from 'npm:@base44/sdk@0.8.25';
-
-const db = createClient({ appId: Deno.env.get('BASE44_APP_ID') });
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.25';
 
 function getCurrentWeekId() {
     const now = new Date();
@@ -13,9 +11,10 @@ function getCurrentWeekId() {
 
 Deno.serve(async (req) => {
     try {
+        const base44 = createClientFromRequest(req);
         const week_id = getCurrentWeekId();
 
-        const bossRecords = await db.entities.GlobalBoss.filter({ week_id });
+        const bossRecords = await base44.asServiceRole.entities.GlobalBoss.filter({ week_id });
         if (bossRecords.length > 0) return Response.json({ boss: bossRecords[0] });
 
         let charCodeSum = 0;
@@ -25,7 +24,7 @@ Deno.serve(async (req) => {
         const bossName = bossNames[charCodeSum % bossNames.length];
         const bossHp = 50000;
 
-        const newBoss = await db.entities.GlobalBoss.create({
+        const newBoss = await base44.asServiceRole.entities.GlobalBoss.create({
             week_id, boss_id: 'world_boss_' + (charCodeSum % 4),
             name: bossName, max_hp: bossHp, current_hp: bossHp,
             reward_type: 'gold', reward_id: '25000',
