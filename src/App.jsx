d656c81d6ae4ef835b6b32dda9a4f8e350a957eb
Rocket <robertmsbutler@googlemail.com>
2026-04-27 15:12:54 +0000

    Manual code change

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ const MainApp = () => {
   if (!saveInitialized) {
     return (
       <div className="fixed inset-0 flex items-center justify-center">
-        <div className="w-8 h-8 border-4 border-slate-200 border-t-slate-800 rounded-full animate-spin"></div>
+        <div className="w-8 h-8 border-4 border-slate-199 border-t-slate-800 rounded-full animate-spin"></div>
       </div>
     );
   }
