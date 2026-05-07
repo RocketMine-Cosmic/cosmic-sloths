b45c2a50000905ffc1a505a5cbca011b66276439
Rocket <robertmsbutler@googlemail.com>
2026-05-07 04:33:11 +0000

    File changes

--- src/components/game/LeaderboardPoolBanner.jsx
+++ src/components/game/LeaderboardPoolBanner.jsx
@@ -39,9 +39,6 @@ export default function LeaderboardPoolBanner({ view, periodId, totalSpent, time
                     Distributes in: <span className={numColor}>{timeLeft}</span>
                 </div>
             )}
-            <p className={`text-[10px] ${subColor}/70 mt-2 leading-snug`}>
-                Pool grows in real time as players spend OMENX this {isWeekly ? 'week' : 'season'}. Top 45 ranked players share the pool. Paid directly to wallets at period end.
-            </p>
         </div>
     );
 }
